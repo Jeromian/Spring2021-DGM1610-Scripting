0bbc36485d6ae8f528f1e6f61db704c751d392e4
Matt <matthewgasser@outlook.com>
2021-02-25 19:59:11 -0700

    Added Head and fixed score bug

--- BallGenerator/Assets/Scripts/Counter.cs
+++ BallGenerator/Assets/Scripts/Counter.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
@@ -9,14 +7,12 @@ public class Counter : MonoBehaviour
     public int creditValue=3;
     public IntData numberCount;
     public Text counterText;
-    
-    // Start is called before the first frame update
+
     void Start()
     {
-        numberCount.value == 0;
+        numberCount.value *= 0;
     }
-
-    // Update is called once per frame
+    
     void OnTriggerEnter2D()
     {
         numberCount.value += creditValue;
