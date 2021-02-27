396ef8ac528a68f3f6a59b0ac951ac2e2ab024b4
Matt <matthewgasser@outlook.com>
2021-02-27 14:12:06 -0700

    Created door and key system

---
+++ BallGenerator/Assets/Scripts/GateOpener.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateOpener : MonoBehaviour
+{
+    public BoolData keyObj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (keyObj.value)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
