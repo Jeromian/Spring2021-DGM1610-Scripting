a9929d33264913e2257090ee4682f753a9376820
Matt <matthewgasser@outlook.com>
2021-03-18 14:48:06 -0600

    Made loop Behavior script

---
+++ BallGenerator/Assets/Scripts/LoopBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopBehavior : MonoBehaviour
+{
+
+    public string[] playerNames;
+    
+    private IEnumerator Start()
+    {
+        foreach (var playerName in playerNames)
+        {
+            print(playerName);
+        }
+
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            print("Hello");
+        }
+        
+    }
+
+}
